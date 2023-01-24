d348ef9a9fbf6e6761e78da6afa3c8f395203187
cuongvu3009 <cuongvu3009@gmail.com>
2023-01-24 13:45:53 +0200

    feat: add login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className='App'>
       <Router>
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </Router>
     </div>
   );
