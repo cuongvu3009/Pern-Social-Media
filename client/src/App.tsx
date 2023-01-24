eb11267ac9e7cb1a888f11b95007750d2817ff41
cuongvu3009 <cuongvu3009@gmail.com>
2023-01-24 13:43:43 +0200

    feat: add home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react';
-import './App.css';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Home />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
